ce04562e57278bc1a45f51f3bc20ef8443fefd10
proabdulbasit <proabdulbasit.me@gmail.com>
2024-01-24 00:08:13 +0500

    fixed the issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,7 @@
 import '@/styles/globals.css'
-import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
-  return
-  <>
-  <Component {...pageProps} />
-  <Analytics />
-  </>
+  return <Component {...pageProps} />
+  
+
 }
